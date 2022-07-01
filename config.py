6ea8eb59eff7b3b61c498885730e879308dc7ca3
Peter Pan <415391360@qq.com>
2022-07-01 18:19:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=ClF34S44TBYnaQgFnqrxBt2; device_id=10012553541647588114748177faa3cd488b6c46e578c0fe89499ed779; __jsluid_s=3f201ec700305bfa3f4ff8478534172f; homepage_sug=d; r_sort_type=score; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1656670118; sensorsdata2015jssdkcross={"distinct_id":"17f9be77b708fd-091cea03987285-1f3e6757-2007040-17f9be77b71b90","first_id":"","props":{"$latest_traffic_source_type":"直接流量","$latest_search_keyword":"未取到值_直接打开","$latest_referrer":""},"$device_id":"17f9be77b708fd-091cea03987285-1f3e6757-2007040-17f9be77b71b90"}; sess=AT-nYMiqXqp3da0FwvFwK28hJ8GOAvKdlp24NVzLoAN3biTquxGU+83sj52P0TGi4r/fqvXxX0MOPfvfHW5NLaYYaon8A4CkvEBenD9kt7s3Qr1+O+GRNvdzR76; user=user:7625505760|7625505760; _zdmA.uid=ZDMA.wpU664eJC.1656670132.2419200; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1656670132; smzdm_id=7625505760'
 
 
 """
